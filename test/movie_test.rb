9e24211d72f4f2571b4399dbd576cad9db3dd5c3
Brian Marshall <bmarsha72@gmail.com>
2016-05-02 09:52:05 -0500

    three examples, no failures

--- test/movie_test.rb
+++ test/movie_test.rb
@@ -9,8 +9,17 @@ require 'httparty'
 describe Movie do
   it "should return a hash" do
     Movie.new.to_hash.class.should == Hash
-
   end
 
+    it "should be Top Gun" do
+      @tg = Movie.new.to_hash
+      @tg ["Title"].should == "Top Gun"
+    end
 
+    it "should be 1986" do
+      @tg = Movie.new.to_hash
+      @tg ["Year"].should == "1986"
+    end
+
+  
 end
